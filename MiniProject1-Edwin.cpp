f989a5c0d07f4153c27fb44fd1c6d245c3d45055
shadow-drive <shadowdrive691@gmail.com>
2015-09-23 16:43:49 +1100

    Create MiniProject1-Edwin.cpp

---
+++ MiniProject1-Edwin.cpp
@@ -0,0 +1,13 @@
+// Submitted By: Mervyn Edwin
+// Submitted To: Edper Castro
+// Mini Project 1
+#include <iostream>
+#include <string>
+
+int main()
+{
+  std::string name;
+  std::cout << "What is your name? ";
+  getline (std::cin, name);
+  std::cout << "Hello, " << name << "!\n";
+}
